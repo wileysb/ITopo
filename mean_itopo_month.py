4ddc0d076f162811f41ca8da3075f7113ac9a2c7
wileysb <wiley.bogren@gmail.com>
2015-11-29 22:09:45 +0100

    script fixes

--- mean_itopo_month.py
+++ mean_itopo_month.py
@@ -1,46 +1,59 @@
 #!/usr/bin/python
 
-__author__ = 'wiley'
-
-import sys, os, glob
+import os
+import sys
+import glob
+import yaml
 import numpy as np
 
-from topocorr_funcs import srb_utm33_dir, srb_3hr_fmt, gdal_load, utm33n_md, \
-    gdal_save_grid, unpack_srb_variables
+from topocorr_funcs import gdal_load, unpack_srb_variables, gdal_save_grid
+
+__author__ = 'wiley'
+
+
+project_name = sys.argv[1]  # $ python accumulate_skyview.py projectName
+
+project_param_fn = '{}_parameters.yaml'.format(project_name)
+with file(project_param_fn) as f:
+    project_parameters = yaml.safe_load(f)
+
 
 if __name__ == "__main__":
-    yyyy=str(sys.argv[1])
-    mm  = str(sys.argv[2]).zfill(2)
+    yyyy = str(sys.argv[2])
+    mm = str(sys.argv[3]).zfill(2)
 
-    out_dir = 'srb_utm_daily/'+yyyy+mm
+    tmp_dir = os.path.join(project_parameters['tmp_dir'], yyyy+mm)
+    in_fmt = os.path.join(tmp_dir, '{0}_{1}_{2}_{3}.tif')  # .format(dset,yyyy,mm,utc_hour)
 
-    srb_3hr_fn = srb_3hr_fmt.format(yyyy,mm)
+    out_fmt = os.path.join(project_parameters['out_dir'], '{0}_{1}_{2.tif')  # .format(dset,yyyy,mm)
 
-    srb_3hr_vars = unpack_srb_variables(srb_3hr_fn)
+    srb_3hr_fn = project_parameters['srb'].format(yyyy, mm)
 
-    ydays     = srb_3hr_vars['ydays']
+    srb_3hr_vars = unpack_srb_variables(srb_3hr_fn, project_parameters)
+
+    ydays = srb_3hr_vars['ydays']
     utc_hours = srb_3hr_vars['utc_hours']
 
     dsets = {}
 
-    for dset in ['itopo','diffuse','sw_sfc_dn']:
-        fn_pattern = os.path.join(out_dir, dset+'_*.tif')
+    for dset in ['itopo', 'diffuse', 'sw_sfc_dn']:
+        fn_pattern = os.path.join(project_parameters['out_dir'], dset+'_*.tif')
         dset_files = glob.glob(fn_pattern)
-        arr = np.ones((len(dset_files), utm33n_md['y_size'], utm33n_md['x_size']),dtype='float')
+        arr = np.ones((len(dset_files), project_parameters['dem_gt']['y_size'],
+                       project_parameters['dem_gt']['x_size']), dtype='float')
         for i in range(len(dset_files)):
             arr[i] = gdal_load(dset_files[i])
-            arr[i][arr[i]<0] = 0
+            arr[i][arr[i] < 0] = 0
 
-        dsets[dset] = np.nanmean(arr,axis=0)
-        utm33n_md['from_dset'] = dsets[dset]
-        utm33n_md['nanhandle'] = -999
-        utm33n_md['outfn'] = os.path.join(srb_utm33_dir,dset+'_'+yyyy+mm+'.tif')
-        gdal_save_grid(**utm33n_md)
-
+        dsets[dset] = np.nanmean(arr, axis=0)
+        project_parameters['dem_gt']['from_dset'] = dsets[dset]
+        project_parameters['dem_gt']['nanhandle'] = -999
+        project_parameters['dem_gt']['outfn'] = out_fmt.format(dset, yyyy, mm)
+        gdal_save_grid(**project_parameters['dem_gt'])
 
     diff = (dsets['itopo'] - dsets['sw_sfc_dn']) / dsets['sw_sfc_dn']
     diff[np.isinf(diff)] = 0
-    utm33n_md['from_dset'] = diff
-    utm33n_md['nanhandle'] = -999
-    utm33n_md['outfn'] = os.path.join(srb_utm33_dir,'irrad_rel_diff_'+yyyy+mm+'.tif')
-    gdal_save_grid(**utm33n_md)
+    project_parameters['dem_gt']['from_dset'] = diff
+    project_parameters['dem_gt']['nanhandle'] = -999
+    project_parameters['dem_gt']['outfn'] = out_fmt.format('irrad_rel_diff', yyyy, mm)
+    gdal_save_grid(**project_parameters['dem_gt'])

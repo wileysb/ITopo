09ef79c943f26ca96733c2c3fc9834fb23748b35
wileysb <wiley.bogren@gmail.com>
2015-11-30 17:33:17 +0100

    cast shade properly

--- README.md
+++ README.md
@@ -3,40 +3,13 @@
 Trying to make the code more streamlined and flexible, rather than locked 
 in extent and resolution.
 
+
 ### Directions for use:
-1'a. Create 'projectName' directory within ITopo/
-1'b. Define 'projectName' parameters in parameters.yaml
- * For best quality control, preload projectName_dem.tif in dem_derivs/. 
- * Then you can also leave blank the project's spatial parameters.
-1'c. run $ python start_project.py projectName
-
-OR
-
-1. run $ python start_from_dem.py projectName /path/to/src_dem.tif
-
-2. run $ parallel -j numThreads -- < projectDir/BOG.cmds
-3. run $ python accumulate_skyview.py projectName
-4. run $ parallel -j numThreads ./gen_sunview.py projectName {1}::: 0 3 6 9 12 15 18 21
-
-Implement workflow: 
-5.  python gen_diff_days.py projectName yyyy mm
-6.  python gen_itopo_days.py projectName yyyy mm
-7.  python mean_itopo_month.py projectName yyyy mm
-8.  Free space with rm -rf projectName/tmp/yyyymm
-
-#### Timing
-Cols x Rows | Step 5^  | Step 6 | Step 7^
------------ | -------- | ------ | -----
- 504 x 504  | 1557min  | ~30min | ~5min
-~~5041 x 5041~~ |Too large!|   ...  | 
-
-^ time can be divided by numThreads
-
-Porsgrunn10m (5041x5041) BOGs is already 30G at azimuth 7/359.  Each BOG.asc is 49M.  This is unmanageably large.
-Maybe more manageable as compressed tifs? for comparison, Norway at 1km (1210x1532) was ~3.6M.
-
-Possible solution: pick a subregion within Porsgrunn at half cols, half rows? or 33% cols and rows...
-
+1. Prepare a dem in the desired output projection and alignment
+2. Check the 5 lines of generic parameters in parameters.yaml
+3. Run $ python start_from_dem.py projectName /path/to/dem.tif
+4. Run $ chmod 744 itopo_1month.sh
+5. Choose a number of processors to tie up, and run the commands prompted at the completion of [4].
 
 
 ### TODO:
--- start_from_dem.py
+++ start_from_dem.py
@@ -173,13 +173,14 @@ else:
     print 'Making Lat and Lon grids in output projection'
     mk_latlon_grids(**latlon_args)
 
-    bash_cmds = ['#!/usr/bin/env bash\n', '\n']
-    months_cmds_fn = os.path.join(project_parameters['itopo_dir'], 'itopo_months_{0}.cmds'.format(project_name))
-    bash_cmds.append('parallel -j $1 -- < {0}/BOG.cmds\n'.format(project_name))
-    bash_cmds.append('python accumulate_skyview.py '+project_name+'\n')
-    bash_cmds.append('parallel -j $1 ./gen_sunview.py '+project_name+' {1}::: 0 3 6 9 12 15 18 21\n')
-    bash_cmds.append('parallel -j $1 -- < {0}\n'.format(months_cmds_fn))
+    # Prepare gen_sunview.py parallel arguments
+    sunview_cmds_fn = os.path.join(project_parameters['itopo_dir'], 'sunview_{0}.cmds'.format(project_name))
+    with open(sunview_cmds_fn, 'w') as f:
+        for utc_hr in range(0, 22, 3):
+            f.writeline('python gen_sunview.py {0} {1}\n'.format(project_name, utc_hr))
 
+    # Prepare itopo_1month.sh parallel arguments
+    months_cmds_fn = os.path.join(project_parameters['itopo_dir'], 'itopo_months_{0}.cmds'.format(project_name))
     months_to_run = []
     for mm in project_parameters['months']:
         for year in range(project_parameters['first_year'], project_parameters['last_year']+1):
@@ -187,6 +188,14 @@ else:
 
     with open(months_cmds_fn, 'w') as f:
         f.writelines(months_to_run)
+
+    # Prepare Stage 2 commands
+    bash_cmds = ['#!/usr/bin/env bash\n', '\n']
+
+    bash_cmds.append('parallel -j $1 -- < {0}/BOG.cmds\n'.format(project_name))
+    bash_cmds.append('python accumulate_skyview.py '+project_name+'\n')
+    bash_cmds.append('parallel -j $1 -- < {0}\n'.format(sunview_cmds_fn))
+    bash_cmds.append('parallel -j $1 -- < {0}\n'.format(months_cmds_fn))
 
     with open(project_name+'.sh', 'w') as f:
         f.writelines(bash_cmds)

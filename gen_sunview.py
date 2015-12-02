450cd3aa6781c941d545fe5204d555c3806f2ffc
wileysb <wiley.bogren@gmail.com>
2015-12-02 17:28:07 +0100

    cleanup

--- gen_sunview.py
+++ gen_sunview.py
@@ -18,41 +18,6 @@ with file(project_param_fn) as f:
     project_parameters = yaml.safe_load(f)
 
 
-
-
-def gdal_save_binary_grid(from_dset, outfn, epsg, x_size, y_size, ulx, uly, dx, dy,
-                          nanhandle=False, rot0=0, rot1=0):
-
-    # Bring projection info from EPSG to WKT string
-    projection = osr.SpatialReference()
-    projection.ImportFromEPSG(epsg)
-    projection_string = projection.ExportToWkt()
-
-     # Create memory dataset
-    if outfn == 'MEM':
-        mem_drv = gdal.GetDriverByName('MEM')
-        dst = mem_drv.Create('', x_size, y_size, 1, gdal.GDT_Byte)
-
-    else:
-        gtiff_drv = gdal.GetDriverByName('GTiff')
-        dst = gtiff_drv.Create(outfn,x_size, y_size, 1, gdal.GDT_Byte)
-
-    # Assemble geotransform
-    geo_t = (ulx, dx, rot0, uly, rot1, dy)
-
-    # Write geotransform, projection, and array to memory dataset
-    dst.SetGeoTransform(geo_t)
-    dst.SetProjection(projection_string)
-    bnd = dst.GetRasterBand(1)
-    if nanhandle is not False:
-        from_dset[np.isinf(from_dset)] = nanhandle
-        from_dset[np.isnan(from_dset)] = nanhandle
-        bnd.SetNoDataValue(nanhandle)
-    bnd.WriteArray(from_dset)
-
-    del dst # return dst
-
-
 if __name__ == "__main__":
     utc_hour = int(sys.argv[2])
 
@@ -73,5 +38,4 @@ if __name__ == "__main__":
         sunview_args['outfn'] = sunview_fmt.format(yday, utc_hour)
         sunview_args['dtype'] = gdal.GDT_Byte
         sunview_args['from_dset'] = Cast_shade(project_parameters, lat, lon, yday, utc_hour).astype('int')
-        #gdal_save_binary_grid(**sunview_args)
         gdal_save_grid(**sunview_args)

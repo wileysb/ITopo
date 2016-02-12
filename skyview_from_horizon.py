b49e1e50ca1f004f661f99ec9dcb6a2e3fe1587a
Wiley Bogren <wib@skogoglandskap.no>
2016-02-12 12:20:12 +0100

    skyview2.0 nearly there

---
+++ skyview_from_horizon.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+"""Define skyview using the horizon grids for az=0:369
+
+The sum of visible angles is weighted
+by cosine(90-zenith) to account for smaller sectors per degree
+as zenith angle approaches noon. This weighted sum, divided by the maximum 
+possible sum, gives the percent sky viewable from each point in the grid"""
+
+import yaml
+import gdal
+import numpy as np
+import os
+import sys
+from topocorr_funcs import gdal_save_grid, gdal_load
+
+project_name = sys.argv[1]  # $ python accumulate_skyview.py projectName
+
+project_param_fn = '{0}/{0}_parameters.yaml'.format(project_name)
+with file(project_param_fn) as f:
+    project_parameters = yaml.safe_load(f)
+
+horizon_fmt = os.path.join(project_parameters['dem_dir'],'horizon','horizon_{0}azi.tif')#.format(azi)
+outfn = os.path.join(project_parameters['dem_dir'],'skyview_horizon.tif')
+
+def sum_azi_slice(horizon_grid):
+    skyview_slice = np.zeros_like(horizon_grid)
+    for altitude in range(0,90):
+        skyview_slice += np.where(horizon_grid<altitude, np.cos(np.deg2rad(altitude)), 0)
+    return skyview_slice
+
+
+slice_max = np.cos(np.deg2rad(np.arange(0,90))).sum()
+
+horizon = gdal_load(horizon_fmt.format(0.0))
+skyview = sum_azi_slice(horizon)/slice_max
+for azimuth in range(1,360):
+    horizon = gdal_load(horizon_fmt.format(float(azimuth)))
+    skyview += sum_azi_slice(horizon)/slice_max
+
+skyview_args = {'x_size': project_parameters['x_size'], 'y_size': project_parameters['y_size'],
+                'ulx': project_parameters['xmin'], 'uly': project_parameters['ymax'],
+                'epsg': project_parameters['epsg'], 'dx': project_parameters['dx'], 'dy': project_parameters['dy'],
+                'outfn': outfn, 'from_dset': skyview}
+gdal_save_grid(**skyview_args)
+
